94065197f5c681cac341b6e7ca44afdf43a03fd1
TomokiYoshizawa <tomoki-goalie@hotmail.com>
2023-11-06 19:59:31 -0800

    add pages directory and routes on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../../team-3-instock-client/src/pages/HomePage/HomePage";
+import WareHouse from "../../team-3-instock-client/src/pages/WareHouse/WareHouse";
+import Inventory from "../../team-3-instock-client/src/pages/Inventry/Inventory";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <p>test</p>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<HomePage />} />
+        <Route path="/warehouse" element={<WareHouse />} />
+        <Route path="/warehouse/:id" element={<WareHouse />} />
+        {/* This route will lead to "Edit Warehouse/Add New WareHouse Page" */}
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/inventory/:id" element={<Inventory />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/WareHouse/WareHouse.jsx
@@ -0,0 +1,7 @@
+import "./WareHouse.scss";
+
+function WareHouse() {
+  return <p>WareHouse Page</p>;
+}
+
+export default WareHouse;
