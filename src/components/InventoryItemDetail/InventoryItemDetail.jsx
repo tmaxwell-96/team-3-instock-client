769cf1134bc3ed3c6a8c5076beac3147763a9102
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-08 11:17:37 -0800

    Add file structure and header structure

---
+++ src/components/InventoryItemDetail/InventoryItemDetail.jsx
@@ -0,0 +1,15 @@
+import "./InventoryItemDetail.scss";
+
+const InventoryItemDetail = () => {
+  return (
+    <div className="item-detail">
+      <div className="item-detail__header">
+        <img src="" alt="back arrow" />
+        <h2 className="item-detail__item">INVENTORY ITEM PLACEHOLDER</h2>
+        <div className="item-detail__edit-wrapepr">
+          <img src="" alt="edit" />
+        </div>
+      </div>
+    </div>
+  );
+};
