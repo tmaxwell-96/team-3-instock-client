f69d0eec272e626b373e5d7e629dbf47f028589e
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-08 11:42:32 -0800

    Add assets and general header styling

--- src/Pages/InventoryPage/InventoryPage.jsx
+++ src/Pages/InventoryPage/InventoryPage.jsx
@@ -1,7 +1,8 @@
 import "./InventoryPage.scss";
+import InventoryItemDetail from "../../components/InventoryItemDetail/InventoryItemDetail";
 
 function InventoryPage() {
-  return <p>Inventory Page</p>;
+  return <InventoryItemDetail />;
 }
 
 export default InventoryPage;
--- src/components/InventoryItemDetail/InventoryItemDetail.jsx
+++ src/components/InventoryItemDetail/InventoryItemDetail.jsx
@@ -1,15 +1,19 @@
 import "./InventoryItemDetail.scss";
+import backArrow from "../../assets/Icons/arrow_back-24px.svg";
+import edit from "../../assets/Icons/edit-24px.svg";
 
 const InventoryItemDetail = () => {
   return (
     <div className="item-detail">
       <div className="item-detail__header">
-        <img src="" alt="back arrow" />
+        <img src={backArrow} alt="back arrow" />
         <h2 className="item-detail__item">INVENTORY ITEM PLACEHOLDER</h2>
-        <div className="item-detail__edit-wrapepr">
-          <img src="" alt="edit" />
+        <div className="item-detail__edit-wrapper">
+          <img className="item-detail__edit" src={edit} alt="edit" />
         </div>
       </div>
     </div>
   );
 };
+
+export default InventoryItemDetail;
