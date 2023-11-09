f8b9e6abbcf0f1538e88307c42df8cf631c735b6
amit-kannojiya <kannojiya.amit1028@gmail.com>
2023-11-09 11:14:46 -0800

    inventory list responsive

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -16,13 +16,34 @@ const InventoryList = () => {
 
   return (
     <div className="inventory-list">
-      <h2 className="inventory-list__header">inventory</h2>
+      <h2 className="inventory-list__header">Inventory</h2>
       <input
         className="inventory-list__search"
         type="text"
-        placeholder="Search"
+        placeholder="Search..."
       />
       <button className="inventory-list__button">+ Add New inventory</button>
+      <h2 class="component__title">List Component</h2>
+      <ul class="component__box component__box--row">
+        <li class="component__subheader component__subheader--row">
+          <span class="component__detail"> INVENTORY ITEM</span>
+        </li>
+        <li class="component__subheader component__subheader--row">
+          <span class="component__detail"> CATEGORY</span>
+        </li>
+        <li class="component__subheader component__subheader--row">
+          <span class="component__detail"> STATUS</span>
+        </li>
+        <li class="component__subheader component__subheader--row">
+          <span class="component__detail"> QUANTITY</span>
+        </li>
+        <li class="component__subheader component__subheader--row">
+          <span class="component__detail"> WAREHOUSE</span>
+        </li>
+        <li class="component__subheader component__subheader--row">
+          <span class="component__detail"> ACTIONS</span>
+        </li>
+      </ul>
       <ul className="inventory-list__wrapper">
         {inventoryList.map((item) => {
           return <InventoryCard key={item.id} item={item} />;
