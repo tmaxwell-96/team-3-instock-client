577fca9217595e45225166bfa723dcd67a95044f
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-08 20:03:01 -0800

    Style component for all devices

--- src/Pages/InventoryPage/InventoryPage.jsx
+++ src/Pages/InventoryPage/InventoryPage.jsx
@@ -1,9 +1,11 @@
 import "./InventoryPage.scss";
 import InventoryList from "../../components/InventoryList/InventoryList";
+import InventoryItemDetail from "../../components/InventoryItemDetail/InventoryItemDetail";
 
 function InventoryPage() {
   return (
     <div>
+      <InventoryItemDetail />
       <InventoryList />
     </div>
   );
--- src/components/InventoryItemDetail/InventoryItemDetail.jsx
+++ src/components/InventoryItemDetail/InventoryItemDetail.jsx
@@ -1,6 +1,6 @@
 import "./InventoryItemDetail.scss";
 import backArrow from "../../assets/Icons/arrow_back-24px.svg";
-import edit from "../../assets/Icons/edit-24px.svg";
+import editWhite from "../../assets/Icons/edit-24px-white.svg";
 
 const InventoryItemDetail = () => {
   return (
@@ -9,9 +9,41 @@ const InventoryItemDetail = () => {
         <img src={backArrow} alt="back arrow" />
         <h2 className="item-detail__item">INVENTORY ITEM PLACEHOLDER</h2>
         <div className="item-detail__edit-wrapper">
-          <svg className="item-detail__edit">
-            <img src={edit} alt="edit" />
-          </svg>
+          <img className="item-detail__edit" src={editWhite} alt="edit" />
+          <p className="item-detail__edit-text">Edit</p>
+        </div>
+      </div>
+      <div className="item-detail__content">
+        <div className="item-detail__content-item">
+          {" "}
+          <label className="item-detail__label">ITEM DESCRIPTION:</label>
+          <p className="item-detail__text">
+            {" "}
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Exercitationem error hic doloremque deserunt pariatur voluptates.
+          </p>
+          <div className="item-detail__content-item-bottom">
+            <label className="item-detail__label">CATEGORY:</label>
+            <p className="item-detail__text">PLACEHOLDER CATEGORY</p>
+          </div>
+        </div>
+        <div className="item-detail__content-status">
+          <div className="item-detail__content-left">
+            <label className="item-detail__label">STATUS:</label>
+            <div className="item-detail__stock-wrapper">
+              <p className="item-detail__text item-detail__text--stock">
+                IN STOCK
+              </p>
+            </div>
+            <div className="item-detail__content-status-bottom">
+              <label className="item-detail__label">WAREHOUSE:</label>
+              <p className="item-detail__text">PLACEHOLDER </p>
+            </div>
+          </div>
+          <div className="item-detail__right">
+            <label className="item-detail__label">QUANTITY:</label>
+            <p className="item-detail__text">PLACEHOLDER</p>
+          </div>
         </div>
       </div>
     </div>
