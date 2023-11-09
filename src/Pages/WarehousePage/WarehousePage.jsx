f4dd5d061a5d4bd3e54d5a40d16588abd5fa1cac
tomokinpm <tomoki-goalie@hotmail.com>
2023-11-09 12:03:44 -0800

    initial commit on my branch

--- src/Pages/WarehousePage/WarehousePage.jsx
+++ src/Pages/WarehousePage/WarehousePage.jsx
@@ -1,9 +1,11 @@
 import "./WarehousePage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import WarehouseDetail from "../../components/WarehouseDetail/WarehouseDetail";
 
 const WarehousePage = () => {
   return (
     <div>
+      <WarehouseDetail />
       <WarehouseList />
     </div>
   );
--- src/components/WarehouseDetail/WarehouseDetail.jsx
+++ src/components/WarehouseDetail/WarehouseDetail.jsx
@@ -1,13 +1,34 @@
-import InputField from "../InputField/InputField";
+import { useState, useEffect } from "react";
 import "./WarehouseDetail.scss";
+import axios from "axios";
 
-function WarehouseDetail() {
+const WarehouseDetail = () => {
+  const [warehouseDetail, setWarehouseDetail] = useState([]);
+  const getWarehouseDetail = async () => {
+    const response = await axios.get("http://localhost:8080/warehouses/");
+    setWarehouseDetail(response.data);
+    // console.log(response.data);
+  };
+  useEffect(() => {
+    getWarehouseDetail();
+  }, []);
+  // console.log(warehouseDetail);
+
   return (
-    <div>
-      {/* <p>test detail component</p>; */}
-      <InputField />
+    <div className="warehouse-detail">
+      {/* <p>warehouse detail test</p> */}
+      <div className="warehouse-detail__address-container">
+        <p className="warehouse-detail__text">WAREHOUSE ADDRESS:</p>
+        <p className="warehouse-detail__text">text:{warehouseDetail[1].id}</p>
+      </div>
+      <div className="warehouse-detail__contact-container">
+        <p className="warehouse-detail__text">CONTACT NAME:</p>
+        <p className="warehouse-detail__text">text:{warehouseDetail[1].id}</p>
+        <p className="warehouse-detail__text">CONTACT INFORMATION:</p>
+        <p className="warehouse-detail__text">text:{warehouseDetail[1].id}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default WarehouseDetail;
