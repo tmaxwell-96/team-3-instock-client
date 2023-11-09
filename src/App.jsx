5b386a94f34634e8233db0db2211970c17f685a9
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-09 15:08:18 -0800

    Ignore .env, correct MainNav order in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,23 @@ import MainNav from "./components/MainNav/MainNav";
 import WarehouseDetail from "./components/WarehouseDetail/WarehouseDetail";
 
 function App() {
-    return (
-        <div>
-       <MainNav />
-            <section className="app__container">
-              
-                <BrowserRouter>
-                    <p>MAIN INFO PLACEHOLDER</p>
-
+  return (
+    <div>
+      <section className="app__container">
+        <BrowserRouter>
+          <MainNav />
 
           <Routes>
             <Route path="/" element={<WarehousePage />} />
             <Route path="/:id" element={<WarehouseDetail />} />
             <Route path="/inventory" element={<InventoryPage />} />
-            <Route path="/inventory/:id" element={<InventoryPage />} />
+            <Route path="/inventory/:id" element={<InventoryItemDetail />} />
           </Routes>
         </BrowserRouter>
       </section>
       <h2>FOOTER PLACEHOLDER</h2>
     </div>
   );
-       
-
+}
 
 export default App;
