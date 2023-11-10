c1b338eb4158c15b42a1dbf3c902298943dfedec
tomokinpm <tomoki-goalie@hotmail.com>
2023-11-09 21:04:54 -0800

    add edit inventory component

--- src/Pages/WarehousePage/WarehousePage.jsx
+++ src/Pages/WarehousePage/WarehousePage.jsx
@@ -2,14 +2,15 @@ import "./WarehousePage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 
 import WarehouseDetail from "../../components/WarehouseDetail/WarehouseDetail";
+import EditInventory from "../../components/EditInventory/EditInventory";
 
 // import MainNav from "../../components/MainNav/MainNav";
-
 
 const WarehousePage = () => {
   return (
     <div>
       {/* <MainNav/> */}
+      <EditInventory />
       <WarehouseList />
     </div>
   );
