fc363060499cdf4e4da0aba363ebdbcc4e81db97
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-10 14:23:03 -0800

    Style for mobile view

--- src/components/WarehouseListCard/WarehouseCard.jsx
+++ src/components/WarehouseListCard/WarehouseCard.jsx
@@ -2,6 +2,7 @@ import "./WarehouseCard.scss";
 import { Link } from "react-router-dom";
 import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import editIcon from "../../assets/Icons/edit-24px.svg";
+import rightArrow from "../../assets/Icons/chevron_right-24px.svg";
 
 const WarehouseCard = ({ warehouse }) => {
   if (warehouse) {
@@ -9,15 +10,16 @@ const WarehouseCard = ({ warehouse }) => {
       <li className="warehouse-card">
         <div className="warehouse-card__left">
           <div className="warehouse-card__left-top">
-            <p className="warehouse-card__text">WAREHOUSE</p>
-            <Link to={`/${warehouse.id}`}>
-              <p className="warehouse-card__text warehouse-card__text--link">
-                {`${warehouse.warehouse_name}   >`}
+            <p className="warehouse-card__label">WAREHOUSE</p>
+            <Link className="warehouse-card__link" to={`/${warehouse.id}`}>
+              <p className="warehouse-card__text">
+                {`${warehouse.warehouse_name}`}
               </p>
+              <img src={rightArrow} alt="" />
             </Link>
           </div>
           <div className="warehouse-card__left-bottom">
-            <p className="warehouse-card__text">ADDRESS</p>
+            <p className="warehouse-card__label">ADDRESS</p>
             <p className="warehouse-card__text ">{warehouse.address}</p>
             <p className="warehouse-card__text ">
               {warehouse.city}, {warehouse.country}
@@ -27,15 +29,19 @@ const WarehouseCard = ({ warehouse }) => {
         </div>
         <div className="warehouse-card__right">
           <div className="warehouse-card__right-top">
-            <p className="warehouse-card__text">CONTACT NAME</p>
+            <p className="warehouse-card__label">CONTACT NAME</p>
             <p className="warehouse-card__text ">{warehouse.contact_name}</p>
           </div>
           <div className="warehouse-card__right-bottom">
-            <p className="warehouse-card__text">CONTACT INFORMATION</p>
+            <p className="warehouse-card__label">CONTACT INFORMATION</p>
             <p className="warehouse-card__text ">{warehouse.contact_phone}</p>
             <p className="warehouse-card__text ">{warehouse.contact_email}</p>
           </div>
-          <img src={editIcon} alt="writing icon" />
+          <img
+            className="warehouse-card__edit"
+            src={editIcon}
+            alt="writing icon"
+          />
         </div>
       </li>
     );
